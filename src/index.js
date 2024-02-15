6774aa2b3bc9d902f8e297f6a7d90a747d05c15a
mansi kathiriya <mansikathiriya08@gmail.com>
2024-02-15 14:04:19 +0530

    add file

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// body parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.use(cookieParser())
+
+// ejs view template
+app.set("view wngine","ejs");
+
+// routes
+app.use("v1", routes)
+
+app.get("/", (req,res,next) => {
+    res.render("index")
+})
+// db
+connectDB();
+
+// server
+http.createServer(app).listen(process.env.PORT, () => {
+    console.log("server started");
+})
