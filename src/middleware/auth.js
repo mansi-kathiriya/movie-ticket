c068e420d09630e30bd67382d0549cddb5343ad6
mansi kathiriya <mansikathiriya08@gmail.com>
2024-02-21 13:55:24 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,32 +12,13 @@ const autheticate = (req, res, next) => {
   }
 
   let user = jwt.verify(token, process.env.SECRET);
-  console.log(user);
 
   req.user = user;
   next();
 
 };
 
-const restrict=(...data)=>{
-
-return (req,res,next)=>{
-
-    let user = req.user
-
-    if(data[0].includes(user.role))
-    {
-        req.user=user
-        next()
-    }
-
-    res.status(400).json({message:"you are not allow"})
-}
-
-}
-
 module.exports = {
   createToken,
   autheticate,
-  restrict
 };
